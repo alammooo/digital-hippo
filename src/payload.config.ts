1c6e63fc39cf71df7f824b45aacd1b9d937ca707
alammooo <abdullah.alamm@gmail.com>
2024-01-24 11:12:09 +0700

    initializing trpc

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -21,7 +21,7 @@ export default buildConfig({
     admin: '/sell',
   },
   admin: {
-    user: 'users',
+    // user: 'users',
     bundler: webpackBundler(),
     meta: {
       titleSuffix: '- DigitalHippo',
